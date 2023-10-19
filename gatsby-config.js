b8910b00b227d60e3b057d2c3062440af868d1f4
connieya <gunny6026@naver.com>
2023-10-19 14:45:37 +0900

    chore : Emotion JS 라이브러리 설정

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,14 +10,15 @@
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `gatsby 로 나만의 블로그 만들기`,
+    author: `Park Geon Hee`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
     `gatsby-plugin-image`,
+    `gatsby-plugin-emotion`,
     {
-      resolve: "gatsby-plugin-typescript",
+      resolve: 'gatsby-plugin-typescript',
       options: {
         isTSX: true,
         allExtensions: true,
