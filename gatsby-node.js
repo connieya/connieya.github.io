6506c00d0f5ef588257dfefd624a351174d9bda5
connieya <gunny6026@naver.com>
2023-10-19 12:26:59 +0900

    chore : 타입스크립트 , eslint 설정

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,6 +4,24 @@
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
  */
 
+const path = require("path")
+
+// Setup Import Alias
+exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
+  const output = getConfig().output || {}
+
+  actions.setWebpackConfig({
+    output,
+    resolve: {
+      alias: {
+        components: path.resolve(__dirname, "src/components"),
+        utils: path.resolve(__dirname, "src/utils"),
+        hooks: path.resolve(__dirname, "src/hooks"),
+      },
+    },
+  })
+}
+
 /**
  * @type {import('gatsby').GatsbyNode['createPages']}
  */
