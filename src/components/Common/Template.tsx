e07b36771c3371138a1dfedd52285d67ace67a61
connieya <gunny6026@naver.com>
2023-10-22 22:09:21 +0900

    feat : Google Search Console 를 위한 메타 테그 추가

--- src/components/Common/Template.tsx
+++ src/components/Common/Template.tsx
@@ -47,6 +47,11 @@ const Template: FunctionComponent<TemplateProps> = function ({
         <meta name="twitter:site" content="@사용자이름" />
         <meta name="twitter:creator" content="@사용자이름" />
 
+        <meta
+          name="google-site-verification"
+          content="MewvnFIOz6F5cDFIX2LfX-KbHvYPNMGHxIemGGdYWSk"
+        />
+
         <html lang="ko" />
       </Helmet>
       <GlobalStyle />
